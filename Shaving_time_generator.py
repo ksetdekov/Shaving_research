fb1b7ec2684da8584a4aca12046c5f1cedf4e9c8
ksetdekov <Qe82SPsYawm2vwFpHrCc>
2020-04-06 14:31:06 +0300

    made Shaving_time_generator.py

---
+++ Shaving_time_generator.py
@@ -0,0 +1,17 @@
+# генерирует список на 7 дней, чем бриться случайно
+# электро или станок
+# для электро - пена или без
+from scipy.stats import bernoulli
+
+p = 0.5  # p of T-handle
+shaver_choice = bernoulli.rvs(p, size=7)
+q = 0.5  # p of no foam
+foam = bernoulli.rvs(q, size=7)
+shavers = ['electric', 'T-handle']
+foam_use = ['with foam', 'without foam']
+for i in range(7):
+    if shaver_choice[i] == 1:
+        foam_res = foam_use[0]
+    else:
+        foam_res = foam_use[foam[i]]
+    print('Day', i + 1, 'shave with', shavers[shaver_choice[i]], foam_res)
